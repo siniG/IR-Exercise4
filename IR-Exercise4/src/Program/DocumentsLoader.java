7fd4831de2598ef3c775ca0c4ae2dd0036415ef6
Amit Dar <dar.amit@gmail.com>
2014-05-24 15:36:53 +0300

    updates to document loader, actual file loading added

--- IR-Exercise4/src/Program/DocumentsLoader.java
+++ IR-Exercise4/src/Program/DocumentsLoader.java
@@ -1,9 +1,7 @@
 package Program;
 
 import java.io.*;
-import java.util.Hashtable;
-import java.util.Iterator;
-import java.util.StringTokenizer;
+import java.util.*;
 
 /**
  * handles all document loading and id handling.
@@ -14,6 +12,7 @@ public class DocumentsLoader implements IDocumentsLoader
     private Hashtable<Integer, String> documentNameByDocumentId;
     private Hashtable<String, Integer> documentIdByDocumentName;
     private Hashtable<Integer, Integer> documentClusterIdByDocumentId;
+    private Hashtable<Integer, String> rawDocumentByDocumentId;
 
     public DocumentsLoader(String documentsFile)
     {
@@ -22,6 +21,7 @@ public class DocumentsLoader implements IDocumentsLoader
         this.documentNameByDocumentId = new Hashtable<Integer, String>();
         this.documentIdByDocumentName = new Hashtable<String, Integer>();
         this.documentClusterIdByDocumentId = new Hashtable<Integer, Integer>();
+        this.rawDocumentByDocumentId = new Hashtable<Integer, String>();
     }
 
     public boolean LoadDocuments()
@@ -35,9 +35,24 @@ public class DocumentsLoader implements IDocumentsLoader
 
             String line;
 
+            // load the file names, their document ids and the their gold standard cluster ids
             while ((line = documentsFileBufferedReader.readLine()) != null)
             {
                 ProcessLineFromDocumentsFile(line);
+            }
+
+            System.out.println("Loaded documents file " + documentsFile + " successfully, " + documentNameByDocumentId.size() + " documents listed in the file.");
+            System.out.println("Starting to load all the documents themselves, this might take some time...");
+
+            // try loading all the actual document data from the disk.
+            if (!LoadDocumentsContentFromFiles())
+            {
+                System.out.println("Not all document were successfully loaded, check your input.");
+            }
+            else
+            {
+                // all the documents form the documents file loaded successfully.
+                result = true;
             }
         }
         catch (Exception e)
@@ -48,6 +63,75 @@ public class DocumentsLoader implements IDocumentsLoader
         return result;
     }
 
+    /**
+     * gets a root folder, and returns all the files in it and in all the subdirectories under it.
+     * @param rootDirectory
+     * @return
+     */
+    private List<File> GetFilesInDirectory(File rootDirectory)
+    {
+        List<File> result = new ArrayList<File>();
+
+        File[] filesArray = rootDirectory.listFiles();
+        for (File currentFile : filesArray)
+        {
+            if (currentFile.isDirectory())
+            {
+                result.addAll(GetFilesInDirectory(currentFile));
+            }
+            else
+            {
+                result.add(currentFile);
+            }
+        }
+
+        return result;
+    }
+
+    private boolean LoadDocumentsContentFromFiles()
+    {
+        boolean result = false;
+
+        String userDirectory = System.getProperty("user.dir");
+        File rootUserDirectory = new File(userDirectory);
+
+        List<File> allFiles = GetFilesInDirectory(rootUserDirectory);
+
+        for (File fileToLoad : allFiles)
+        {
+            if (documentIdByDocumentName.containsKey(fileToLoad.getName()))
+            {
+                // this file should be loaded since it represents a document
+                int currentFileDocumentId = documentIdByDocumentName.get(fileToLoad.getName());
+
+                try
+                {
+                    Scanner fileScanner = new Scanner(fileToLoad);
+                    String entireFileContent = fileScanner.next();
+                    if (entireFileContent.length() == 0)
+                    {
+                        System.out.println("No content found for file: " + fileScanner.toString());
+                    }
+                    else
+                    {
+                        // file should be loaded since it is a document, and it has content.
+                        rawDocumentByDocumentId.put(currentFileDocumentId, entireFileContent);
+                    }
+                }
+                catch (FileNotFoundException e)
+                {
+                    System.out.println("Unable to load file: " + fileToLoad.toString());
+                }
+            }
+        }
+
+        if (rawDocumentByDocumentId.size() == documentNameByDocumentId.size())
+        {
+            // this means we successfully loaded all the documents we received in the docs file parameter.
+            result = true;
+        }
+        return result;
+    }
     /**
      * processes a single line from the documents file according to the following format:
      * <document id> <file name> <gold standard cluster id>
