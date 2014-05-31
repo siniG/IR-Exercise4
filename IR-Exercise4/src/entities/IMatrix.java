88106f079d55a0098ca90e39001b10a8030f1fbd
Avi Gabay <sini@avis-MacBook-Pro.local>
2014-05-31 17:02:01 +0300

    add column/row size to matrix

--- IR-Exercise4/src/entities/IMatrix.java
+++ IR-Exercise4/src/entities/IMatrix.java
@@ -5,5 +5,7 @@ public interface IMatrix {
 	public boolean set(int row, int col, Double c);
 	public Double get(int row, int col);
 	public double[] getRow(int row);
+	public int getColumnsNumber();
+	public int getRowsNumber();
 	public void init();
 }
