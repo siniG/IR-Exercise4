107cb86f8802fdf3a58fd2821676004db57ea8d8
Amit Dar <dar.amit@gmail.com>
2014-06-07 15:42:54 +0300

    remove unused shit from utils

--- IR-Exercise4/src/utilities/utils.java
+++ IR-Exercise4/src/utilities/utils.java
@@ -1,46 +1,7 @@
 package utilities;
 
-import java.io.IOException;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-
-import org.apache.commons.math3.linear.RealVector;
-
-public class utils {
-
-	public static double CalculateHypotenuseFromOrigin(double cosineAngle, double adjacentDistance)
-	{
-		
-		double result;
-		
-		if(cosineAngle == 0.0 || Double.isNaN(cosineAngle)|| Double.isNaN(adjacentDistance))
-		{
-			result = Double.NaN;
-		}
-		else
-		{
-			result = adjacentDistance / cosineAngle;
-		}
-		
-		return result;
-	}
-	
-	public static double[] floatArrayToDoubleArray(float[] array)
-	{
-		if(array == null)
-			return null;
-		
-		double[] dArray = new double[array.length];
-		
-		for(int i = 0; i<array.length; i++)
-		{
-			dArray[i] = array[i];
-		}
-		
-		return dArray;
-	}
-
+public class utils
+{
     public static double cosineSimilarity(float[] vectorA, float[] vectorB)
     {
         double dotProduct = 0.0;
