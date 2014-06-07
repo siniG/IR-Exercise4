574094dcd4d132a26ba99a450210e0118ba90a32
Amit Dar <dar.amit@gmail.com>
2014-06-07 11:49:27 +0300

    added cosine similarity calculation in utils.

--- IR-Exercise4/src/utilities/utils.java
+++ IR-Exercise4/src/utilities/utils.java
@@ -33,4 +33,17 @@ public class utils {
 		
 		return dArray;
 	}
+
+    public static double cosineSimilarity(double[] vectorA, double[] vectorB)
+    {
+        double dotProduct = 0.0;
+        double normA = 0.0;
+        double normB = 0.0;
+        for (int i = 0; i < vectorA.length; i++) {
+            dotProduct += vectorA[i] * vectorB[i];
+            normA += (vectorA[i] * vectorA[i]);
+            normB += (vectorB[i] * vectorB[i]);
+        }
+        return dotProduct / (Math.sqrt(normA) * Math.sqrt(normB));
+    }
 }
